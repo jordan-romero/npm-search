c52440dfd8c41f83560f10cdbdfa72a5ff26fbac
jordles113 <jordan.t.romero@gmail.com>
2022-05-16 13:47:37 -0500

    finishes up redux/ts practice

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,23 +1,29 @@
-import { useState } from 'react'
-import { useDispatch } from 'react-redux';
-import { actionCreators } from '../state';
+import { useState } from 'react';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+  const { searchRepositories } = useActions();
+  const { data, error, loading} = useTypedSelector((state) => state.repositories)
 
-  const [term, setTerm] = useState('')
-  const dispatch = useDispatch()
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    dispatch(actionCreators.searchRepositories(term))
-  }
+    event.preventDefault();
+
+    searchRepositories(term);
+  };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input value={term} onChange={e => setTerm(e.target.value)} />
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && 
+        data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
